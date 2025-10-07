cee7cc2e777e9ebbfb27af0d9b39c17efa21d0b4
DESKTOP-KFK8OMP\34610 <franciscoalia1992@gmail.com>
2025-10-07 18:59:40 +0200

    Django Proyecto de Radio, esqueleto. No esta terminado. Ejercicio futuro para practicar comandos y orm

---
+++ RadioVirgenDeGracia2026/radioapp/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from datetime import date
+
+# Create your models here.
+class Direccion(models.Model):
+    CIUDADES = ['CIUDAD REAL', 'ARGAMASILLA', 'PUERTOLLANO', 'ALMODOVAR']
+    calle = models.CharField(max_length=10)
+    numero = models.IntegerField()
+    ciudad = models.CharField(choices=CIUDADES)
+
+
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    nick = models.CharField(max_length=20, unique=True)
+    fecha_nacimiento = models.DateField()
+    telefono = models.CharField(max_length=12)
+    email = models.EmailField()
+    direccion = models.ForeignKey(Direccion,on_delete=models.CASCADE)
+    activo = models.BooleanField(default=True)
+
+    def calcular_edad(self):
+        hoy = date.today()
+        edad = hoy.year - self.fecha_nacimiento.year
+        if (hoy.month, hoy.day) < (self.fecha_nacimiento.month, self.fecha_nacimiento.day):
+            edad -= 1
+        return edad
+
+    def __str__(self):
+        return f"{self.id} {self.nick}"
+
+class Cancion(models.Model):
+    nombre = models.CharField(max_length=20)
+    ano_lanzamiento = models.IntegerField()
+    duracion = models.IntegerField()
+
+class Autor(models.Model):
+    GENEROS = ['Pop','Rap','Reggeton','Electronica']
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=20)
+    genero = models.CharField(choices=GENEROS,max_length=12)
+
+
